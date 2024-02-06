bff3abf790b8e07b49d3e9bb3c79d6c5ab08fed9
kimyoungwoong <yw2023727@gmail.com>
2024-02-06 14:54:13 +0900

    POST, PUT, DELETE 구현

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,37 @@ export function GET(request: NextRequest) {
     { id: 2, name: "B"},
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  if (!body.name) {
+    return NextResponse.json({error: "Name is required"}, {status:404});
+  }
+
+  return NextResponse.json({id: 1, name:body.name});
+}
+
+export async function PUT(request: NextRequest, {params} : {params: {id: number}}) {
+
+  const body = await request.json();
+
+  if (!body.name) {
+    return NextResponse.json({error: "Name is required"}, {status:404});
+  }
+
+  if (params.id > 10) {
+    return NextResponse.json({error: "User Not Found"}, {status:404});
+  }
+  
+  return NextResponse.json({id: 1, name:body.name});
+}
+
+export async function DELETE(request: NextRequest, {params} : {params: {id: number}}) {
+
+  if (params.id > 10) {
+    return NextResponse.json({error: "User Not Found"}, {status:404});
+  }
+  
+  return NextResponse.json({});
+}
