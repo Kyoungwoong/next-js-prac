e0d4a6d9bca9a5fc308226f7c3e3456ab671ca4a
kimyoungwoong <yw2023727@gmail.com>
2024-02-07 18:14:28 +0900

    이미지 업로드

---
+++ app/upload/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from 'react'
+import { CldUploadWidget } from 'next-cloudinary'
+
+const UploadPage = () => {
+  return (
+    <CldUploadWidget uploadPreset='nsvybcqv'>
+        {({ open }) => (
+            <button className="btn btn-primary" onClick={() => open()}>
+                Upload an Image
+            </button>
+        )}
+    </CldUploadWidget>
+    )
+}
+
+export default UploadPage
