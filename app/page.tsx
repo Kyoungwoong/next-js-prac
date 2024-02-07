6dcd14217c736fd5ea08bdc5144387f2bdcfbfbc
Kyoungwoong <kyy980708@gmail.com>
2024-02-07 22:21:36 +0900

    서버단에서 세션 데이터에 접근하기

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import ProductCard from "@/components/ProductCard";
 import Image from "next/image";
 import Link from "next/link";
+import {authOptions} from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
       <h1>
-        Hello, world!
+        안녕하세요 - {session && <span>{session.user!.name}</span>}님! 반갑습니다.
       </h1>
       {/* <a href="/users">users</a> */}
       <Link href ="/users">USERS</Link>
