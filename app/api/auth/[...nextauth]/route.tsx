ca562e797b57719fa171b671bb330c26627e1072
Kyoungwoong <kyy980708@gmail.com>
2024-02-07 22:03:53 +0900

    구글 provider 적용

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,7 +1,18 @@
 import NextAuth from "next-auth"
 
-const handler = NextAuth({});
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+});
 
 export {handler as GET, handler as POST};
 
 // 그리고 env에 nextauth url / secrete 첨부
+/*
+ * GOOGLE_CLIENT_ID= 1067623416159-o5re6lamqdnouhfcn2p8nav3svqg4ok1.apps.googleusercontent.com
+ * GOOGLE_CLIENT_SECRET= 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
+ */
